123cfbd2e94eebd3b9a451502c3bd4f88ea77d89
hongnha0586 <nhale.31241020586@st.ueh.edu.vn>
2026-03-27 15:02:08 +0700

    b

--- TechStore.Tests/MockData/MockProducts.cs
+++ TechStore.Tests/MockData/MockProducts.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using TechStore.Models;
 
 namespace TechStore.Tests.MockData
 {
-    internal class MockProducts
+    public static class MockProducts
     {
+        public static List<Product> GetProducts()
+        {
+            return new List<Product>
+            {
+                new Product { ProductId = 1, ProductName = "Laptop", Price = 1000, StockQuantity = 10 },
+                new Product { ProductId = 2, ProductName = "Mouse", Price = 50, StockQuantity = 20 },
+                new Product { ProductId = 3, ProductName = "Keyboard", Price = 100, StockQuantity = 15 }
+            };
+        }
     }
 }
