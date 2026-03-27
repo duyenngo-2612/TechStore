d2eeef5bcfc5435b9965bf911ceff7dcc7875ab1
hongnha0586 <nhale.31241020586@st.ueh.edu.vn>
2026-03-27 16:46:35 +0700

    customweb

---
+++ TechStore.Tests/IntegrationSetup/CustomWebApplicationFactory.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using TechStore.Models;
+
+namespace TechStore.Tests.IntegrationSetup
+{
+    public class CustomWebApplicationFactory<TProgram>
+        : WebApplicationFactory<TProgram> where TProgram : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                // Xóa DbContext thật (SQL Server)
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions<TechStoreContext>));
+
+                if (descriptor != null)
+                {
+                    services.Remove(descriptor);
+                }
+
+                // Thêm InMemory Database
+                services.AddDbContext<TechStoreContext>(options =>
+                {
+                    options.UseInMemoryDatabase("TechStoreTestDb");
+                });
+
+                var sp = services.BuildServiceProvider();
+
+                using (var scope = sp.CreateScope())
+                {
+                    var db = scope.ServiceProvider.GetRequiredService<TechStoreContext>();
+                    db.Database.EnsureCreated();
+                }
+            });
+        }
+    }
+}
